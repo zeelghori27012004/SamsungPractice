00546dfe38499d6ed92dd0fe9a091fe9d39c135e
Zeel Ghori <ghorizeeljkvrz@gmail.com>
2025-10-08 01:03:21 +0530

    Update sum level K.cpp

--- sum level K.cpp
+++ sum level K.cpp
@@ -1,82 +1,90 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-typedef long long int LL;
+int main() {
+    // NOTE: For robust code, use std::vector and long long (LL) for values/sum
+    int arr_level[100] = {0}; // store levels (int is fine)
+    int arr_value[100] = {0}; // store node values (int used as per original)
+    int size = 0;             // number of nodes stored
 
-#define scll(x) scanf("%lld",&x);
-#define sci(x) scanf("%d",&x);
-#define prll(x) printf("%lld\n",x);
-#define pri(x) printf("%d\n",x);
+    int x;       // input level
+    string str;  // tree string
 
-/*
-3 
-(0(5(6()())(-4()(9()())))(7(1()())(3()())))
-*/
+    if (!(cin >> x >> str)) return 1;
 
-int main()
-{
-	int arr[2][100]={0};	
-	int size = 0;
-	int x;
-	string str;
-	
-	cin>>x;
-	cin>>str;
-	int level=0, temp=0, commit=0, neg=0;
-	for(int i=0; i<str.length(); i++){
-		
-		if(str[i]=='(' || str[i]==')'){
-			if(commit == 1){
-				if(neg==1){
-					temp=temp*-1;
-					neg=0;
-				}
-				
-				arr[0][size]=level;
-				arr[1][size]=temp;
-				size++;
-				temp=0;		
-			}
-			
-			if(str[i]=='('){			
-				level++;
-			}
-			else if(str[i]==')'){
-				level--;
-			}
-			commit=0;
-		}
-		else if(str[i]=='-'){
-			neg=1;
-			commit = 1;
-		}
-		else{
-			temp= temp*10 + str[i] - '0';
-			commit = 1;
-		}	
-			
-	}
+    int level = 0;      // current depth, starts at 0, root value stored at level 1
+    int temp = 0;       // current number being read
+    bool commit = false; // true if a number is in progress/just finished
+    bool neg = false;    // true if number is negative
 
-	int temp0,temp1;
-	for(int i=0;i<size-1;i++){
-		for(int j=i+1; j<size; j++){
-			if(arr[0][j] > arr[0][i]){
-				temp0=arr[0][j];
-				temp1=arr[1][j];
-				arr[0][j]=arr[0][i];
-				arr[1][j]=arr[1][i];
-				arr[0][i]=temp0;
-				arr[1][i]=temp1;
-			}
-			
-		}
-	}
-	
-	int sum = 0;
-	for(int i=0;i<size;i++){
-		if(arr[0][i]==x+1)	sum+=arr[1][i];
-	}
-	
-	cout<<sum<<endl;
-	return 0;
+    for (int i = 0; i < str.length(); i++) {
+        char c = str[i];
+
+        if (c == '(') {
+            // 1️⃣ Handle open bracket '('
+            
+            // If a number was just read (e.g., '0(' or '5(')
+            if (commit) {
+                // Finalize and store the value with the current level
+                if (neg) temp *= -1;
+                
+                arr_level[size] = level; // Store the current level
+                arr_value[size] = temp;
+                size++;
+                
+                // Reset for the next node's value
+                temp = 0;
+                neg = false;
+                commit = false;
+            }
+
+            level++; // Always go down a level
+        }
+        else if (c == ')') {
+            // 2️⃣ Handle close bracket ')'
+            
+            // A closing bracket signifies the end of a subtree.
+            // If the closing bracket immediately follows a value (which only 
+            // happens if the value is part of the original error), it's a structural 
+            // issue. In a valid tree string (V(L)(R)), the value V is followed 
+            // by '('. We simply move up the tree structure.
+            
+            // This is the CRITICAL change: ONLY DECREMENT LEVEL.
+            // No storage of value should happen here.
+            
+            // If the node was a null child represented by "()", commit is false.
+            if (commit) {
+                // This block should ideally not be reached in a correctly structured loop,
+                // but we keep it clean.
+                if (neg) temp *= -1;
+                // Since this is the end of the recursion, the node value must have 
+                // been read previously. No storage here.
+            }
+            
+            level--; // Always go up a level
+            commit = false;
+        }
+        else if (c == '-') {
+            // 3️⃣ Handle negative sign
+            neg = true;
+            commit = true;
+        }
+        else if (isdigit(c)) {
+            // 4️⃣ Handle digit
+            temp = temp * 10 + (c - '0');
+            commit = true;
+        }
+    }
+
+    // Calculate sum at level x + 1
+    int sum = 0;
+    int target_level = x + 1;
+
+    for (int i = 0; i < size; i++) {
+        if (arr_level[i] == target_level)
+            sum += arr_value[i];
+    }
+
+    cout << sum << endl;
+    return 0;
 }
